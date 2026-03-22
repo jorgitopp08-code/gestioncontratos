567db60fa031a637ad91d0ab6891ea0bbd722b88
jorgitopp08-code <jorgitopp08@gmail.com>
2026-03-22 12:13:58 -0500

    tercer cambio

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "sql210.infinityfree.com";
 $user = "if0_41392256";
 $pass = "7gLLLrvpdiB";
-$db = "if0_41392256_gestion_contratos";
+$db = "if0_41392256_apijorge_empleados";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
