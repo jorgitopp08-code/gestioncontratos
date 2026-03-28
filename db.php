229911447b663a9abf2c418f03e5330dd5ca7260
jorgitopp08-code <jorgitopp08@gmail.com>
2026-03-28 10:20:24 -0500

    mas cambios

--- db.php
+++ db.php
@@ -1,12 +1,16 @@
 <?php
-$host = "mysql-apijorge.alwaysdata.net";
-$user = "apijorge";
-$pass = "clase1234";
-$db = "apijorge_gestion_empleados";
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$conn = new mysqli($host, $user, $pass, $db);
+try {
+    $host = "mysql-apijorge.alwaysdata.net";
+    $user = "apijorge";
+    $pass = "clase1234";
+    $db = "apijorge_gestion_empleados";
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    $conn = new mysqli($host, $user, $pass, $db, 3306);
+    $conn->set_charset("utf8mb4");
+
+} catch (mysqli_sql_exception $e) {
+    die("Error de conexión: " . $e->getMessage());
 }
 ?>
